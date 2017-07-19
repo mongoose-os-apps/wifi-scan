38f53b4faf4b84f4e874bb65cb1ad75e1443e56f
Deomid Ryabkov <rojer@cesanta.com>
2017-07-19 09:40:01 +0100

    ESP32 Ethernet support; mgos_net_* API
    Networking API is generalized to mgos_net_*, in particular state change notification and IP address information retrieval.
    
    PUBLISHED_FROM=b312fd620dadc14183640b1caa158d011415b0eb

--- src/main.c
+++ src/main.c
@@ -14,15 +14,17 @@ static void button_cb(int pin, void *arg) {
   mgos_wifi_scan(wifi_scan_cb, NULL);
 }
 
-static void wifi_cb(enum mgos_wifi_status event, void *arg) {
-  LOG(LL_INFO, ("WiFi change event: %d, arg %p", (int) event, arg));
+static void wifi_cb(enum mgos_net_event ev,
+                    const struct mgos_net_event_data *ev_data, void *arg) {
+  if (ev_data->if_type != MGOS_NET_IF_TYPE_WIFI) return;
+  LOG(LL_INFO, ("WiFi change event: %d, arg %p", (int) ev, arg));
 }
 
 enum mgos_app_init_result mgos_app_init(void) {
   int pin = get_cfg()->my_app.button_pin;
   mgos_gpio_set_button_handler(pin, MGOS_GPIO_PULL_UP, MGOS_GPIO_INT_EDGE_POS,
                                200, button_cb, NULL);
-  mgos_wifi_add_on_change_cb(wifi_cb, NULL);
+  mgos_net_add_event_handler(wifi_cb, NULL);
   LOG(LL_INFO, ("Button is configured on GPIO %d.", pin));
   LOG(LL_INFO, ("Click on a button to initialise WiFi scan."));
   return MGOS_APP_INIT_SUCCESS;
