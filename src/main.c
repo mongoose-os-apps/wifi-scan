93cfd84c0e34053ce89093cba852520cf6a91eb7
Dmitry Frank <mail@dmitryfrank.com>
2017-12-22 16:57:20 +0200

    BREAKING: Move mgos_net API to mgos_event
    `mgos_net_add_event_handler()` is removed in favor of `mgos_event`-based
    API.
    
    Example:
    ```c
    static void my_net_ev_handler(int ev, void *evd, void *arg) {
      if (ev == MGOS_NET_EV_IP_ACQUIRED) {
        LOG(LL_INFO, ("Just got IP!"));
        // Fetch something very useful from somewhere
      }
      (void) evd;
      (void) arg;
    }
    
    // Somewhere else:
    mgos_event_add_group_handler(MGOS_EVENT_GRP_NET, my_net_ev_handler, NULL);
    ```
    
    PUBLISHED_FROM=3e468cd9d0698d49713e57429776c29c624b1325

--- src/main.c
+++ src/main.c
@@ -14,8 +14,9 @@ static void button_cb(int pin, void *arg) {
   mgos_wifi_scan(wifi_scan_cb, NULL);
 }
 
-static void wifi_cb(enum mgos_net_event ev,
-                    const struct mgos_net_event_data *ev_data, void *arg) {
+static void wifi_cb(int ev, void *evd, void *arg) {
+  const struct mgos_net_event_data *ev_data =
+      (const struct mgos_net_event_data *) evd;
   if (ev_data->if_type != MGOS_NET_IF_TYPE_WIFI) return;
   LOG(LL_INFO, ("WiFi change event: %d, arg %p", (int) ev, arg));
 }
@@ -24,7 +25,7 @@ enum mgos_app_init_result mgos_app_init(void) {
   int pin = mgos_sys_config_get_my_app_button_pin();
   mgos_gpio_set_button_handler(pin, MGOS_GPIO_PULL_UP, MGOS_GPIO_INT_EDGE_POS,
                                200, button_cb, NULL);
-  mgos_net_add_event_handler(wifi_cb, NULL);
+  mgos_event_add_group_handler(MGOS_EVENT_GRP_NET, wifi_cb, NULL);
   LOG(LL_INFO, ("Button is configured on GPIO %d.", pin));
   LOG(LL_INFO, ("Click on a button to initialise WiFi scan."));
   return MGOS_APP_INIT_SUCCESS;
