62e379e2dd04ca7235f2ffb11d8217ffbab8cda4
Deomid Ryabkov <rojer@cesanta.com>
2019-04-17 14:37:19 +0100

    RS14100: Fix wifi scan error handling
    CL: none
    
    PUBLISHED_FROM=a900dce27735652bf55c6e14faf8df6ebb94b795

--- src/main.c
+++ src/main.c
@@ -13,28 +13,14 @@ static void wifi_scan_cb(int n, struct mgos_wifi_scan_result *res, void *arg) {
   LOG(LL_INFO, ("WiFi scan done."));
 }
 
-static void button_cb(int pin, void *arg) {
-  char buf[8];
-  LOG(LL_INFO, ("Button press on pin %s, arg %p. Starting WiFi scan.",
-                mgos_gpio_str(pin, buf), arg));
+static void timer_cb(void *arg) {
+  LOG(LL_INFO, ("Starting WiFi scan"));
   mgos_wifi_scan(wifi_scan_cb, NULL);
-}
-
-static void wifi_cb(int ev, void *evd, void *arg) {
-  const struct mgos_net_event_data *ev_data =
-      (const struct mgos_net_event_data *) evd;
-  if (ev_data->if_type != MGOS_NET_IF_TYPE_WIFI) return;
-  LOG(LL_INFO, ("WiFi change event: %d, arg %p", (int) ev, arg));
+  (void) arg;
 }
 
 enum mgos_app_init_result mgos_app_init(void) {
-  char buf[8];
-  int btn_pin = mgos_sys_config_get_board_btn1_pin();
-  mgos_gpio_set_button_handler(btn_pin, MGOS_GPIO_PULL_UP,
-                               MGOS_GPIO_INT_EDGE_POS, 200, button_cb, NULL);
-  mgos_event_add_group_handler(MGOS_EVENT_GRP_NET, wifi_cb, NULL);
-  LOG(LL_INFO,
-      ("Button is configured on pin %s.", mgos_gpio_str(btn_pin, buf)));
-  LOG(LL_INFO, ("Click on a button to initialise WiFi scan."));
+  mgos_set_timer(mgos_sys_config_get_scan_interval_ms(),
+                 MGOS_TIMER_REPEAT | MGOS_TIMER_RUN_NOW, timer_cb, NULL);
   return MGOS_APP_INIT_SUCCESS;
 }
